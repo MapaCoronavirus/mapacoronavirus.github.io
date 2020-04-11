ec0b4c22a4321977c5bed08d6a1082a9b005255b
Anderson Ismael <55637907+aicoutos@users.noreply.github.com>
2020-04-11 10:05:34 -0300

    arquivo estados alterado

--- js/estados.js
+++ js/estados.js
@@ -3,162 +3,216 @@ var estados={
         name:"Acre",
         location:[-9.11, -70.52],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "AL":{
         name:"Alagoas",
         location:[-9.57, -36.55],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "AM":{
         name:"Amazonas",
         location:[-4.13, -64.54],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "AP":{
         name:"Amapá",
         location:[1.39, -52],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "BA":{
         name:"Bahia",
         location:[-12.52, -41.69],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "CE":{
         name:"Ceará",
         location:[-5.2, -39.3],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "DF":{
         name:"Distrito Federal",
         location:[-15.795, -47.757778],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "ES":{
         name:"Espirito Santo",
         location:[-20.266667, -40.283333],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "GO":{
         name:"Goiás",
         location:[-16.03, -49.56],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "MA":{
         name:"Maranhão",
         location:[-5.08, -45.14],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "MG":{
         name:"Minas Gerais",
         location:[-18.55, -44.55],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "MS":{
         name:"Mato Grosso do Sul",
         location:[-20.29, -54.87],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "MT":{
         name:"Mato Grosso",
         location:[-12.99, -55.99],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "PA":{
         name:"Pará",
         location:[-3.95, -53.09],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "PB":{
         name:"Paraíba",
         location:[-7.162778, -36.825],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "PE":{
         name:"Pernambuco",
         location:[-8.34, -37.81],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "PI":{
         name:"Piauí",
         location:[-7.42, -42.9],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "PR":{
         name:"Paraná",
         location:[-24.6, -51.383333],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "RJ":{
         name:"Rio de Janeiro",
         location:[-22.916667, -43.183333],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "RN":{
         name:"Rio Grande do Norte",
         location:[-5.74, -36.55],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "RO":{
         name:"Rondônia",
         location:[-10.9, -62.76],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "RR":{
         name:"Roraima",
         location:[2.05, -61.4],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "RS":{
         name:"Rio Grande do Sul",
         location:[-30, -53],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "SC":{
         name:"Santa Catarina",
         location:[-27.27, -50.49],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "SE":{
         name:"Sergipe",
         location:[-10.56, -37.36],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "SP":{
         name:"São Paulo",
         location:[-21.829722, -49.2075],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     },
     "TO":{
         name:"Tocantins",
         location:[-10.19, -48.17],
         casos:0,
-        mortes:0
+        curados:0,
+        mortes:0,
+        suspeitos:0
     }
 };
